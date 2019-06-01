759d295c4afc3590df8b4752adb3bdf11443b4e0
110611061106 <43435589+110611061106@users.noreply.github.com>
2019-06-02 01:08:25 +0800

    printtest

--- src/com/wyx/javaSE/Print1.java
+++ src/com/wyx/javaSE/Print1.java
@@ -3,5 +3,8 @@ package com.wyx.javaSE;
 public class Print1 {
     public static void main(String[] args) {
         System.out.println("Print!");
+        System.out.println("Printwe!");
+        System.out.println("Print!we");
+        System.out.println("Printwqe!");
     }
 }
