bcd9037db32f6a046ae7185f3acf98635790e959
110611061106 <43435589+110611061106@users.noreply.github.com>
2019-06-09 22:29:10 +0800

    0609

--- src/com/wyx/javaSE/Print1.java
+++ src/com/wyx/javaSE/Print1.java
@@ -1,10 +1,20 @@
 package com.wyx.javaSE;
 
+import java.util.Scanner;
+
 public class Print1 {
     public static void main(String[] args) {
         System.out.println("Print!");
-        System.out.println("Printwe!");
-        System.out.println("Print!we");
-        System.out.println("Printwqe!");
+        System.out.println("Hello World!/n 你好");
+        Scanner in = new Scanner(System.in);
+        int amount = 100;
+        int price = 0;
+        System.out.print("请输入票面：");
+        amount = in.nextInt();
+        System.out.print("请输入金额：");
+        price = in.nextInt();
+        //括号内自动计算
+        System.out.println(amount+"-"+price+"=" + (amount-price));
+
     }
 }
